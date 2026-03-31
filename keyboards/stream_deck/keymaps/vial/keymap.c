399c1fe9a633079461c2accadda68bec44c1ed58
Plamen Andreev <plam4u@me.com>
2026-03-31 23:29:44 +0200

    Fix encoder

--- keyboards/stream_deck/keymaps/vial/keymap.c
+++ keyboards/stream_deck/keymaps/vial/keymap.c
@@ -9,30 +9,36 @@
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    // [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    // [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
-    // [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),           ENCODER_CCW_CW(RGB_SPD, RGB_SPI)  },
-    // [3] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),          ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
-    //                  Encoder 1                                     Encoder 2
+    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [2] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [3] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
-        QK_BOOT,      KC_1   ,    KC_2,    KC_3,    KC_4,             KC_5,
-        KC_A   ,      KC_B   ,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,
+        QK_BOOT,      _______,   TG(1),   TG(2),   TG(3),             KC_MUTE,
+        KC_A   ,      KC_1   ,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,
+        KC_I   ,      KC_J   ,    KC_K,    KC_L,    KC_M,    KC_N,    KC_O,    KC_P,
+        KC_Q   ,      KC_R   ,    KC_S,    KC_T,    KC_U,    KC_V,    KC_W,    KC_X
+    ),
+    [1] = LAYOUT(
+        QK_BOOT,      DF(0)  , _______,   TG(2),   TG(3),             KC_MUTE,
+        KC_A   ,      KC_2   ,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,
+        KC_I   ,      KC_J   ,    KC_K,    KC_L,    KC_M,    KC_N,    KC_O,    KC_P,
+        KC_Q   ,      KC_R   ,    KC_S,    KC_T,    KC_U,    KC_V,    KC_W,    KC_X
+    ),
+    [2] = LAYOUT(
+        QK_BOOT,      DF(0)  ,   TG(1), _______,   TG(3),             KC_MUTE,
+        KC_A   ,      KC_3   ,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,
+        KC_I   ,      KC_J   ,    KC_K,    KC_L,    KC_M,    KC_N,    KC_O,    KC_P,
+        KC_Q   ,      KC_R   ,    KC_S,    KC_T,    KC_U,    KC_V,    KC_W,    KC_X
+    ),
+    [3] = LAYOUT(
+        QK_BOOT,      DF(0)  ,   TG(1),   TG(2), _______,             KC_MUTE,
+        KC_A   ,      KC_4   ,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,
         KC_I   ,      KC_J   ,    KC_K,    KC_L,    KC_M,    KC_N,    KC_O,    KC_P,
         KC_Q   ,      KC_R   ,    KC_S,    KC_T,    KC_U,    KC_V,    KC_W,    KC_X
     )
 };
+
